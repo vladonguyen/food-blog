5412aae4e8a14d421653fb473c9d534dcf0d7bff
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2023-12-17 16:21:10 +0200

    Server Seeddata added

--- server/server.js
+++ server/server.js
@@ -69,7 +69,6 @@
     const { ServiceError: ServiceError$1 } = errors;
 
 
-
     function createHandler(plugins, services) {
         return async function handler(req, res) {
             const method = req.method;
@@ -1345,7 +1344,59 @@
     	}
     };
     var seedData = {
- 
+    	recipes: {
+    		"3987279d-0ad4-4afb-8ca9-5b256ae3b298": {
+    			_ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+    			name: "Easy Lasagna",
+    			img: "assets/lasagna.jpg",
+    			ingredients: [
+    				"1 tbsp Ingredient 1",
+    				"2 cups Ingredient 2",
+    				"500 g  Ingredient 3",
+    				"25 g Ingredient 4"
+    			],
+    			steps: [
+    				"Prepare ingredients",
+    				"Mix ingredients",
+    				"Cook until done"
+    			],
+    			_createdOn: 1613551279012
+    		},
+    		"8f414b4f-ab39-4d36-bedb-2ad69da9c830": {
+    			_ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+    			name: "Grilled Duck Fillet",
+    			img: "assets/roast.jpg",
+    			ingredients: [
+    				"500 g  Ingredient 1",
+    				"3 tbsp Ingredient 2",
+    				"2 cups Ingredient 3"
+    			],
+    			steps: [
+    				"Prepare ingredients",
+    				"Mix ingredients",
+    				"Cook until done"
+    			],
+    			_createdOn: 1613551344360
+    		},
+    		"985d9eab-ad2e-4622-a5c8-116261fb1fd2": {
+    			_ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+    			name: "Roast Trout",
+    			img: "assets/fish.jpg",
+    			ingredients: [
+    				"4 cups Ingredient 1",
+    				"1 tbsp Ingredient 2",
+    				"1 tbsp Ingredient 3",
+    				"750 g  Ingredient 4",
+    				"25 g Ingredient 5"
+    			],
+    			steps: [
+    				"Prepare ingredients",
+    				"Mix ingredients",
+    				"Cook until done"
+    			],
+    			_createdOn: 1613551388703
+    		}
+    	},
     	comments: {
     		"0a272c58-b7ea-4e09-a000-7ec988248f66": {
     			_ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
@@ -1355,7 +1406,177 @@
     			_id: "0a272c58-b7ea-4e09-a000-7ec988248f66"
     		}
     	},
+    	records: {
+    		i01: {
+    			name: "John1",
+    			val: 1,
+    			_createdOn: 1613551388703
+    		},
+    		i02: {
+    			name: "John2",
+    			val: 1,
+    			_createdOn: 1613551388713
+    		},
+    		i03: {
+    			name: "John3",
+    			val: 2,
+    			_createdOn: 1613551388723
+    		},
+    		i04: {
+    			name: "John4",
+    			val: 2,
+    			_createdOn: 1613551388733
+    		},
+    		i05: {
+    			name: "John5",
+    			val: 2,
+    			_createdOn: 1613551388743
+    		},
+    		i06: {
+    			name: "John6",
+    			val: 3,
+    			_createdOn: 1613551388753
+    		},
+    		i07: {
+    			name: "John7",
+    			val: 3,
+    			_createdOn: 1613551388763
+    		},
+    		i08: {
+    			name: "John8",
+    			val: 2,
+    			_createdOn: 1613551388773
+    		},
+    		i09: {
+    			name: "John9",
+    			val: 3,
+    			_createdOn: 1613551388783
+    		},
+    		i10: {
+    			name: "John10",
+    			val: 1,
+    			_createdOn: 1613551388793
+    		}
+    	},
+    	catches: {
+    		"07f260f4-466c-4607-9a33-f7273b24f1b4": {
+    			_ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+    			angler: "Paulo Admorim",
+    			weight: 636,
+    			species: "Atlantic Blue Marlin",
+    			location: "Vitoria, Brazil",
+    			bait: "trolled pink",
+    			captureTime: 80,
+    			_createdOn: 1614760714812,
+    			_id: "07f260f4-466c-4607-9a33-f7273b24f1b4"
+    		},
+    		"bdabf5e9-23be-40a1-9f14-9117b6702a9d": {
+    			_ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+    			angler: "John Does",
+    			weight: 554,
+    			species: "Atlantic Blue Marlin",
+    			location: "Buenos Aires, Argentina",
+    			bait: "trolled pink",
+    			captureTime: 120,
+    			_createdOn: 1614760782277,
+    			_id: "bdabf5e9-23be-40a1-9f14-9117b6702a9d"
+    		}
+    	},
+    	furniture: {
+    	},
+    	orders: {
+    	},
+    	movies: {
+    		"1240549d-f0e0-497e-ab99-eb8f703713d7": {
+    			_ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+    			title: "Black Widow",
+    			description: "Natasha Romanoff aka Black Widow confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Comes on the screens 2020.",
+    			img: "https://miro.medium.com/max/735/1*akkAa2CcbKqHsvqVusF3-w.jpeg",
+    			_createdOn: 1614935055353,
+    			_id: "1240549d-f0e0-497e-ab99-eb8f703713d7"
+    		},
+    		"143e5265-333e-4150-80e4-16b61de31aa0": {
+    			_ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+    			title: "Wonder Woman 1984",
+    			description: "Diana must contend with a work colleague and businessman, whose desire for extreme wealth sends the world down a path of destruction, after an ancient artifact that grants wishes goes missing.",
+    			img: "https://pbs.twimg.com/media/ETINgKwWAAAyA4r.jpg",
+    			_createdOn: 1614935181470,
+    			_id: "143e5265-333e-4150-80e4-16b61de31aa0"
+    		},
+    		"a9bae6d8-793e-46c4-a9db-deb9e3484909": {
+    			_ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+    			title: "Top Gun 2",
+    			description: "After more than thirty years of service as one of the Navy's top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.",
+    			img: "https://i.pinimg.com/originals/f2/a4/58/f2a458048757bc6914d559c9e4dc962a.jpg",
+    			_createdOn: 1614935268135,
+    			_id: "a9bae6d8-793e-46c4-a9db-deb9e3484909"
+    		}
+    	},
     	likes: {
+    	},
+    	ideas: {
+    		"833e0e57-71dc-42c0-b387-0ce0caf5225e": {
+    			_ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+    			title: "Best Pilates Workout To Do At Home",
+    			description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima possimus eveniet ullam aspernatur corporis tempore quia nesciunt nostrum mollitia consequatur. At ducimus amet aliquid magnam nulla sed totam blanditiis ullam atque facilis corrupti quidem nisi iusto saepe, consectetur culpa possimus quos? Repellendus, dicta pariatur! Delectus, placeat debitis error dignissimos nesciunt magni possimus quo nulla, fuga corporis maxime minus nihil doloremque aliquam quia recusandae harum. Molestias dolorum recusandae commodi velit cum sapiente placeat alias rerum illum repudiandae? Suscipit tempore dolore autem, neque debitis quisquam molestias officia hic nesciunt? Obcaecati optio fugit blanditiis, explicabo odio at dicta asperiores distinctio expedita dolor est aperiam earum! Molestias sequi aliquid molestiae, voluptatum doloremque saepe dignissimos quidem quas harum quo. Eum nemo voluptatem hic corrupti officiis eaque et temporibus error totam numquam sequi nostrum assumenda eius voluptatibus quia sed vel, rerum, excepturi maxime? Pariatur, provident hic? Soluta corrupti aspernatur exercitationem vitae accusantium ut ullam dolor quod!",
+    			img: "./images/best-pilates-youtube-workouts-2__medium_4x3.jpg",
+    			_createdOn: 1615033373504,
+    			_id: "833e0e57-71dc-42c0-b387-0ce0caf5225e"
+    		},
+    		"247efaa7-8a3e-48a7-813f-b5bfdad0f46c": {
+    			_ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+    			title: "4 Eady DIY Idea To Try!",
+    			description: "Similique rem culpa nemo hic recusandae perspiciatis quidem, quia expedita, sapiente est itaque optio enim placeat voluptates sit, fugit dignissimos tenetur temporibus exercitationem in quis magni sunt vel. Corporis officiis ut sapiente exercitationem consectetur debitis suscipit laborum quo enim iusto, labore, quod quam libero aliquid accusantium! Voluptatum quos porro fugit soluta tempore praesentium ratione dolorum impedit sunt dolores quod labore laudantium beatae architecto perspiciatis natus cupiditate, iure quia aliquid, iusto modi esse!",
+    			img: "./images/brightideacropped.jpg",
+    			_createdOn: 1615033452480,
+    			_id: "247efaa7-8a3e-48a7-813f-b5bfdad0f46c"
+    		},
+    		"b8608c22-dd57-4b24-948e-b358f536b958": {
+    			_ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+    			title: "Dinner Recipe",
+    			description: "Consectetur labore et corporis nihil, officiis tempora, hic ex commodi sit aspernatur ad minima? Voluptas nesciunt, blanditiis ex nulla incidunt facere tempora laborum ut aliquid beatae obcaecati quidem reprehenderit consequatur quis iure natus quia totam vel. Amet explicabo quidem repellat unde tempore et totam minima mollitia, adipisci vel autem, enim voluptatem quasi exercitationem dolor cum repudiandae dolores nostrum sit ullam atque dicta, tempora iusto eaque! Rerum debitis voluptate impedit corrupti quibusdam consequatur minima, earum asperiores soluta. A provident reiciendis voluptates et numquam totam eveniet! Dolorum corporis libero dicta laborum illum accusamus ullam?",
+    			img: "./images/dinner.jpg",
+    			_createdOn: 1615033491967,
+    			_id: "b8608c22-dd57-4b24-948e-b358f536b958"
+    		}
+    	},
+    	catalog: {
+    		"53d4dbf5-7f41-47ba-b485-43eccb91cb95": {
+    			_ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+    			make: "Table",
+    			model: "Swedish",
+    			year: 2015,
+    			description: "Medium table",
+    			price: 235,
+    			img: "./images/table.png",
+    			material: "Hardwood",
+    			_createdOn: 1615545143015,
+    			_id: "53d4dbf5-7f41-47ba-b485-43eccb91cb95"
+    		},
+    		"f5929b5c-bca4-4026-8e6e-c09e73908f77": {
+    			_ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+    			make: "Sofa",
+    			model: "ES-549-M",
+    			year: 2018,
+    			description: "Three-person sofa, blue",
+    			price: 1200,
+    			img: "./images/sofa.jpg",
+    			material: "Frame - steel, plastic; Upholstery - fabric",
+    			_createdOn: 1615545572296,
+    			_id: "f5929b5c-bca4-4026-8e6e-c09e73908f77"
+    		},
+    		"c7f51805-242b-45ed-ae3e-80b68605141b": {
+    			_ownerId: "847ec027-f659-4086-8032-5173e2f9c93a",
+    			make: "Chair",
+    			model: "Bright Dining Collection",
+    			year: 2017,
+    			description: "Dining chair",
+    			price: 180,
+    			img: "./images/chair.jpg",
+    			material: "Wood laminate; leather",
+    			_createdOn: 1615546332126,
+    			_id: "c7f51805-242b-45ed-ae3e-80b68605141b"
+    		}
     	},
     	teams: {
     		"34a1cab1-81f1-47e5-aec3-ab6c9810efe1": {
@@ -1436,7 +1657,7 @@
                 "desc": "My blog as of jan 2022 is hosted on Bigscoots. Before that Nov 2017 onwards was hosted on WP Engine. Before that it was hosted on Bluehost servers for many years with a self-hosted WordPress engine and it uses Foodie theme. Do not worry if these words do not mean much. I did not know them too. And that is why I wanted to write this post. Read on.",
                 "articleContent": "My blog as of jan 2022 is hosted on Bigscoots. Before that Nov 2017 onwards was hosted on WP Engine. Before that it was hosted on Bluehost servers for many years with a self-hosted WordPress engine and it uses Foodie theme. Do not worry if these words do not mean much. I did not know them too. And that is why I wanted to write this post. Read on.\n\nI started my blog on blogger.com as that is what I knew about at the time. I was blogging infrequently, taking bad pictures and the recipes were written in any which way I felt like. Fast forward to some years down the line, when I started taking better pictures, when readers started finding me online, started cooking my food. In those years, I learnt to write better, learnt to develop recipes better, learnt photography all by myself and was often faced with the question of sticking it out on blogger or moving to wordpress. Eventually I moved to self hosted wordpress in 2014. Yes, before that it was all blogger.\n\nWhether you want to start a blog to post food, thoughts, other interests, whether you want to keep is small and personal or make a larger responsive website to reach a larger audience, you can do it much more easily now with numerous options available.",
                 "date": "2023-12-09",
-                "authorName": "Peter Lee",
+                "authorName": "Gabriela Ilieva",
                 "_createdOn": 1702087783862,
                 "_id": "240c440a-bca5-45be-b987-1fea1a433ebd"
             },
@@ -1448,7 +1669,7 @@
                 "desc": "This guide explains how to go vegan with minimal effort, in a lasting and healthy way. I’ve followed a vegan diet for more than thirty years, so I can offer tons of useful advice. By the time you finish reading, you’ll know exactly how to move forward.                ",
                 "articleContent": "Go about things the wrong way, and your vegan transition will demand massive willpower and struggle. This happens when you cut a bunch of non-vegan foods out of your diet, without having anything ready to eat instead. You’ll end up trying replacements you don’t like, having non-fulfilling meals, and generally feeling dissatisfied with your new diet.\n\nOr you can take the easy approach, which requires no sacrifice whatsoever.\n\nHere’s all you need to do to have an enjoyable and effortless transition: don’t seek to cut animal products out of your diet—instead, crowd them out.",
                 "date": "2023-12-09",
-                "authorName": "Peter Lee",
+                "authorName": "Gabriela Ilieva",
                 "_createdOn": 1702087783862,
                 "_id": "140c440a-bca5-45be-b987-1fea1a433ebd"
             }
